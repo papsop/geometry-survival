c938e52da4833893e3e29e16cc8263078ae59c17
Patrik Papso <papsop@outlook.com>
2022-01-24 16:42:05 +0100

    ini config testing

--- DungeonsAndGeometry/src/Game.cpp
+++ DungeonsAndGeometry/src/Game.cpp
@@ -1,10 +1,18 @@
 #include "GameInjection.h"
+
+#include <windows.h>
+#include <WinBase.h>
 #include <iostream>
 
 int main()
 {
-    Game::GameInjection inj;
-    Engine::Application::Instance().Run(inj);
+    //Game::GameInjection inj;
+    //Engine::Application::Instance().Run(inj);
+
+    int number = GetPrivateProfileInt(L"settings", L"action_fire", 0, L"./config.ini");
+    std::cout << number << std::endl;
+    std::cin.get();
+//    WritePrivateProfileString(L"settings", L"action_fire", L"1337", L"./config.ini");
 
     return 0;
 }
